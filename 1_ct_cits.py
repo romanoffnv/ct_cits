a77552f30472f47ac89e94365bff149730624ed7
romanoffnv <romanoffnv@gmail.com>
2022-07-15 09:11:09 +0300

    Plates and indeces

--- 1_ct_cits.py
+++ 1_ct_cits.py
@@ -37,18 +37,19 @@ def main():
 
     L_replace = ['+', '(ПС)', '86-', ')']
     
-    for i in L_replace:
-        L_vehicles = [''.join(str(x).split(str(i))) for x in L_vehicles]    
-    # L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
-    # L_vehicles = [str(x).split('+') for x in L_vehicles]
-    # L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
-    # L_vehicles = [str(x).split('(ПС)') for x in L_vehicles]
-    # L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
-    # L_vehicles = [str(x).split('86-') for x in L_vehicles]
-    # L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
-    # L_vehicles = [str(x).split(')') for x in L_vehicles]
-    # L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
-    # L_vehicles = [str(x).strip() for x in L_vehicles]
+    # for i in L_replace:
+        # L_vehicles = [''.join(str(x).split(str(i))) for x in L_vehicles]    
+        # L_vehicles = str(L_vehicles)
+    L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
+    L_vehicles = [str(x).split('+') for x in L_vehicles]
+    L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
+    L_vehicles = [str(x).split('(ПС)') for x in L_vehicles]
+    L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
+    L_vehicles = [str(x).split('86-') for x in L_vehicles]
+    L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
+    L_vehicles = [str(x).split(')') for x in L_vehicles]
+    L_vehicles = list(itertools.chain.from_iterable(L_vehicles))
+    L_vehicles = [str(x).strip() for x in L_vehicles]
     print(len(L_vehicles))
     L_vehicles = [x for x in L_vehicles if re.findall("\d+", x) or x == '*****']
     L_stars = [x for x in L_vehicles if x == '*****']
@@ -69,23 +70,31 @@ def main():
     L_vehicles = [x.replace('\n', ' ') for x in L_vehicles]
     L_vehicles = [x.replace('трал', 'трал-') for x in L_vehicles]
         
-    plates1 = re.compile("(\d{4})")    
-    plates2 = re.compile("(\d{3})")    
-    # plates1 = re.compile("[А-Яа-я]*\d+[А-Яа-я]{2}\s*\d+")
-    # plates2 = re.compile("[А-Яа-я]{2}\d+\s\d+")
-    # plates3 = re.compile("\w{2}\s\D\d+\s\d{2}")
-    # plates4 = re.compile("\d+\s*[А-Яа-я]{2}\s*\d+")
-    # plates5 = re.compile("[А-Яа-я]{2}\s*\d+\s*\d+")
-    # plates6 = re.compile("\d")
-    # plates7 = re.compile("[А-Яа-я]\s*\d+\s*\W+\s*\d+")
+    indeces1 = re.compile("(\d{4})")    
+    indeces2 = re.compile("(\d{3})")    
+    plates1 = re.compile("[А-Яа-я]*\d+[А-Яа-я]{2}\s*\d+")
+    plates2 = re.compile("[А-Яа-я]{2}\d+\s\d+")
+    plates3 = re.compile("\w{2}\s\D\d+\s\d{2}")
+    plates4 = re.compile("\d+\s*[А-Яа-я]{2}\s*\d+")
+    plates5 = re.compile("[А-Яа-я]{2}\s*\d+\s*\d+")
+    plates6 = re.compile("\d")
+    plates7 = re.compile("[А-Яа-я]\s*\d+\s*\W+\s*\d+")
 
     L_plates = [''.join(re.findall(plates1, x)) or 
-                ''.join(re.findall(plates2, x)) 
+                ''.join(re.findall(plates2, x)) or 
+                ''.join(re.findall(plates3, x)) or 
+                ''.join(re.findall(plates4, x)) or 
+                ''.join(re.findall(plates5, x)) or 
+                ''.join(re.findall(plates6, x)) or 
+                ''.join(re.findall(plates7, x))  
+                for x in L_vehicles]
+    L_indeces = [''.join(re.findall(indeces1, x)) or 
+                ''.join(re.findall(indeces2, x)) 
                 for x in L_vehicles]
 
-    L_plates = [x.replace('186', '') for x in L_plates]
+    L_indeces = [x.replace('186', '') for x in L_indeces]
     
-    data = pd.DataFrame(zip(L_vehicles, L_plates))
+    data = pd.DataFrame(zip(L_vehicles, L_plates, L_indeces))
     pd.set_option("display.max_rows", None, "display.max_columns", None)
     print(data)
     # pprint(L_vehicles)
